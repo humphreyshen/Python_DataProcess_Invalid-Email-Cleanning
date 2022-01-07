4abaac8b1a3b80c046a962008f0cb97cda2b2ff8
Humphrey Shen <92772809+humphreyshen@users.noreply.github.com>
2022-01-08 00:56:05 +0800

    main

--- 2022-01-07-birthday_email_drop_invalid.py
+++ 2022-01-07-birthday_email_drop_invalid.py
@@ -1,12 +1,21 @@
 import pandas as pd
 import numpy as np
 
-personal=pd.read_csv('/Users/humphreyshen/Desktop/2021 Database Project/001總表最新資料/2022-01-07_updated_csv/dDt_alumnidb_personal.csv')
-org_gmail=pd.read_csv('/Users/humphreyshen/Desktop/2021 Database Project/003數據預處理/Birthday_email/2022-01-07_export_invalid_email/生日賀卡錯誤Email名單匯出2021-12-30.csv')
+personal=pd.read_csv('D:/Visual Code/Python/Python_Data_Cleaning_Birthday_Email/drive-download-20220107T142459Z-001/01-07_updated_csv拷貝/dDt_alumnidb_personal.csv')
+org_gmail=pd.read_csv('D:/Visual Code/Python/Python_Data_Cleaning_Birthday_Email/drive-download-20220107T142459Z-001/生日賀卡錯誤Email名單匯出2021-12-30拷貝.csv')
 
 invalid_email=org_gmail['Email'].replace("(錯誤)",'',regex=True).str.replace("(","",regex=True).str.replace(")","",regex=True)
 
-for i in personal['email']:
+result=[]
+for i in personal.email:
     for x in invalid_email:
         if i == x:
-         print(i) 
+           result.append(i)
+
+result_frame=pd.DataFrame(result,columns=['email'])
+
+final_result=personal.merge(result_frame, on='email', how='inner')
+
+final_result_id=final_result.loc[:,['id']].drop_duplicates(subset=['id']).values.tolist()
+
+print('update dDt_alumnidb_personal set email=NULL where id=%s' %final_result_id)
