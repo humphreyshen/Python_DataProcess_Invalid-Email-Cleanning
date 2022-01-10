f468a92d38c52abcd622465fdc8477deb42cdb0e
humphreyshen <humphreyshen1003@gmail.com>
2022-01-10 14:46:32 +0800

    main

--- 2022-01-07-birthday_email_drop_invalid.py
+++ 2022-01-07-birthday_email_drop_invalid.py
@@ -1,8 +1,36 @@
 import pandas as pd
 import numpy as np
 
-personal=pd.read_csv('D:/Visual Code/Python/Python_Data_Cleaning_Birthday_Email/drive-download-20220107T142459Z-001/01-07_updated_csv拷貝/dDt_alumnidb_personal.csv')
-org_gmail=pd.read_csv('D:/Visual Code/Python/Python_Data_Cleaning_Birthday_Email/drive-download-20220107T142459Z-001/生日賀卡錯誤Email名單匯出2021-12-30拷貝.csv')
+print('\n\n==============================================\n歡迎使用由HumphreyShen所設計之電子信箱比對程式\n==============================================\n')
+print('請依序輸入「預計比對的檔案路徑」與「與經過整理的失效電子信箱檔案路徑」')
+print('請注意，本程式所截取之檔案預設為CSV檔！如您輸入錯誤系統會主動提醒您！')
+
+Original_Path=()
+Compare_Path=()
+
+while True:
+    temp_input_1 = input ('\n\n >> 請先輸入您預計更新的檔案路徑：')
+    if (temp_input_1 != 'Y') and ('.csv' in temp_input_1):
+        Original_Path=temp_input_1
+        print('\n初步驗證正確，請在下方鍵入"Y"以做為確認')
+    elif temp_input_1 == 'Y':
+        break
+    else:
+        print('您似乎輸入了錯誤路徑或檔案，請檢查後重新輸入！\n')
+
+while True:
+    temp_input_2 = input('\n\n >> 請輸入您整理彙整失效電子信箱的檔案路徑：')
+    if (temp_input_2 != 'Y') and ('.csv' in temp_input_2):
+        Compare_Path=temp_input_2
+        print('\n初步驗證正確，請在下方鍵入"Y"以做為確認')
+    elif temp_input_2 == 'Y':
+        break
+    else:
+        print('您似乎輸入了錯誤路徑或檔案，請檢查後重新輸入！\n')
+    
+
+personal=pd.read_csv(Original_Path)
+org_gmail=pd.read_csv(Compare_Path)
 
 invalid_email=org_gmail['Email'].replace("(錯誤)",'',regex=True).str.replace("(","",regex=True).str.replace(")","",regex=True)
 
@@ -18,4 +46,5 @@ final_result=personal.merge(result_frame, on='email', how='inner')
 
 final_result_id=final_result.loc[:,['id']].drop_duplicates(subset=['id']).values.tolist()
 
-print('update dDt_alumnidb_personal set email=NULL where id=%s' %final_result_id)
+print('\n\n\n--------以下編號是由系統為您比對出需要刪除之錯誤電子信箱的ID序號--------')
+print(final_result_id)
